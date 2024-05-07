2ca0f7042642c049ffb0073e715c31539a1f3bd0
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-05-07 12:13:40 +0200

    responsive rabbithole

---
+++ src/app/about-me/text-container/text-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-container.component.html',
+  styleUrl: './text-container.component.scss'
+})
+export class TextContainerComponent {
+
+}
