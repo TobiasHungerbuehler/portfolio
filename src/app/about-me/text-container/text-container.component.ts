866b94b8f15e51a6317fdd87a313023c4eb4b98a
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-05-16 12:22:01 +0200

    aboute me component done

--- src/app/about-me/text-container/text-container.component.ts
+++ src/app/about-me/text-container/text-container.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class TextContainerComponent {
 
+  textP1 = "Hi, I'm Tobias, a front-end developer based in St. Gallen, Switzerland, who thrives on solving problems and digitalizing business processes. With a robust background in technical and sales environments, I bring a unique perspective to digital development, combining analytical problem-solving with a communicative approach to deliver straightforward solutions.";
+
+  textP2 = "My programming journey began with a quest to enhance business operations, quickly evolving into a passion for leveraging web technologies to simplify complex processes and innovate digital interactions.";
+
+  textP3 = "I develop user-friendly interfaces and robust backend systems to solve problems and improve workflows. I'm actively expanding my backend skills to tackle complex challenges more effectively.";
 }
