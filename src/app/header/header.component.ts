14d35afb2d6e50e67dd2ef16b82cd356a82b7f58
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-05-23 19:01:26 +0200

    text service done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { NavigationLink } from '../interfaces/navigation-interface.';
 import { NavigationService } from '../services/navigation.service';
 import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+import { TextService } from '../services/text.service';
 
 @Component({
   selector: 'app-header',
@@ -18,11 +19,23 @@ import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 export class HeaderComponent implements OnInit {
 
   links: NavigationLink[] = [];
+
+  currentLanguage = 'EN';
   
   @ViewChild('mobileMenuComponent', { static: false }) 
   private mobileMenu!: MobileMenuComponent;
 
-  constructor(private navigationService: NavigationService) {}
+  constructor(private navigationService: NavigationService, private textService: TextService) {
+        // Abonniere die aktuelle Sprache, um Änderungen zu überwachen
+        this.textService.currentLanguage$.subscribe(lang => {
+          this.currentLanguage = lang;
+        });
+  }
+
+    // Methode, um die Sprache zu setzen
+    setLanguage(language: string) {
+      this.textService.setLanguage(language);
+    }
 
   ngOnInit() {
     this.links = this.navigationService.getLinks();
