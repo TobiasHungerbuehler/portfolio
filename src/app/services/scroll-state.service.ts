f63e1530b6b4402a43af28d565fb5535ff28c6ef
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-05-28 12:50:19 +0200

    navigation component done

--- src/app/services/scroll-state.service.ts
+++ src/app/services/scroll-state.service.ts
@@ -10,17 +10,20 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root' // Specifies that this service should be created by the root application injector.
 })
 export class ScrollStateService {
+
   /**
    * A BehaviorSubject that holds the current active section ID.
    * BehaviorSubject keeps the latest value cached and emits it to new subscribers.
    */
   private sectionSource = new BehaviorSubject<string>('');
 
+
   /**
    * An Observable derived from the sectionSource BehaviorSubject.
    * Components can subscribe to this Observable to get updates about the current active section.
    */
   currentSection = this.sectionSource.asObservable();
+
 
   /**
    * Checks if the given element is visible in the viewport and emits the section ID if it is.
@@ -30,7 +33,6 @@ export class ScrollStateService {
   checkAndEmitVisibility(element: ElementRef, sectionId: string) {
     // Gets the element's current position relative to the viewport
     const rect = element.nativeElement.getBoundingClientRect();
-    
     // Determines if the top of the element is below the top of the viewport and
     // the bottom of the element is above the bottom of the viewport
     const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
