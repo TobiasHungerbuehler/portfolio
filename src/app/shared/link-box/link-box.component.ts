3df0a701ec17ce556c94179969691cc5b310abfc
TobiasHungerbuehler <to.hungerbuehler@gmail.com>
2025-02-10 09:27:18 +0100

    update

---
+++ src/app/shared/link-box/link-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './link-box.component.html',
+  styleUrl: './link-box.component.scss'
+})
+export class LinkBoxComponent {
+
+}
