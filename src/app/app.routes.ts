0d7be2457649a3c9b8bef548d69f4337fee7a550
TobiasHungerbuehler <to.hungerbuehler@gmail.com>
2025-03-10 19:57:33 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
-
 // app.routes.ts
-import { Routes } from '@angular/router';
-import { BaseComponent } from './base/base.component';
+import { Routes } from "@angular/router";
+import { BaseComponent } from "./base/base.component";
+import { ImpressumComponent } from "./impressum/impressum.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
 
 export const routes: Routes = [
-    {path: '', component: BaseComponent,   }
+    { path: "", component: BaseComponent },
+    { path: "impressum", component: ImpressumComponent },
+    { path: "privacy", component: PrivacyComponent },
 ];
